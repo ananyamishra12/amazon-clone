3ce1794b83b521432f8c831ff9d8aa196be77f94
ananyamishra12 <66728375+ananyamishra12@users.noreply.github.com>
2021-10-07 17:47:50 +0530

    header update

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,20 @@
 import React from 'react'
 import logo from '../images/amazon-logo.png'
 import "./Header.css"
+import {Link} from 'react-router-dom'
 function Header() {
     return (
         <nav className="header">
-            {/** amazon logo */}
-            <img className="header__logo" src={logo}
-            alt="amazon logo"
-            />
+            <Link to="/">
+                {/** amazon logo */}
+                <img className="header__logo" src={logo}
+                alt="amazon logo"
+                />
+            </Link>
             
             {/** search bar */}
+            <input type="text" className= "header_searchInput"/>
+
             {/** three links */}
             {/** basket with a number */}
         </nav>
