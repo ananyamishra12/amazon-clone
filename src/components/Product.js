e7ed728525d14d5dadac6544e3cb26926c94e7f5
ananyamishra12 <66728375+ananyamishra12@users.noreply.github.com>
2021-10-15 19:20:58 +0530

    product

--- src/components/Product.js
+++ src/components/Product.js
@@ -4,7 +4,18 @@ function Product({id, title, price, rating, image}) {
     return (
         <div className="product">
             <p>{title}</p>
-            
+            <p>
+                <small>₹</small>
+                <strong>{price}</strong>
+            </p>
+            <div className="product__rating">
+                {
+                    Array(rating).fill()
+                    .map((_)=>
+                    <p>⭐</p>)
+                
+                }
+            </div>
         </div>
     )
 }
