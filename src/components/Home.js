391b137b05da87ec551a20b4b4dde7f70a7f8b84
ananyamishra12 <66728375+ananyamishra12@users.noreply.github.com>
2021-10-15 18:55:55 +0530

    banner completed

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,6 +5,7 @@ function Home() {
         <div className="home">
             <img className="home__image"
             src="https://m.media-amazon.com/images/I/71X3JrrpPwL._SX3000_.jpg" href="banner image"></img>
+            
         </div>
     )
 }
