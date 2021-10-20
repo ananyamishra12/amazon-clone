7cd8a5a046f9051d46d7730e1320dfe8542c6af1
ananyamishra12 <66728375+ananyamishra12@users.noreply.github.com>
2021-10-20 23:50:20 +0530

    product

--- src/components/Home.js
+++ src/components/Home.js
@@ -21,7 +21,39 @@ function Home() {
                 rating= {4}
                 image="https://m.media-amazon.com/images/I/31LC-FBgpBL._AC_SY200_.jpg" />
             </div>
-            
+
+            <div className="home__row">
+                <Product
+                    id = "1"
+                    title = "boAt Airdopes 441 TWS Ear-Buds with IWP Technology, Immersive Audio, Up to 30H Total Playback, IPX7 Water Resistance, Super Touch Controls, Secure Sports Fit & Type-C Port(Active Black)"
+                    price = {900}
+                    rating= {4}
+                    image="https://m.media-amazon.com/images/I/31LC-FBgpBL._AC_SY200_.jpg" />
+
+                    <Product
+                    id = "1"
+                    title = "boAt Airdopes 441 TWS Ear-Buds with IWP Technology, Immersive Audio, Up to 30H Total Playback, IPX7 Water Resistance, Super Touch Controls, Secure Sports Fit & Type-C Port(Active Black)"
+                    price = {900}
+                    rating= {4}
+                    image="https://m.media-amazon.com/images/I/31LC-FBgpBL._AC_SY200_.jpg" />
+
+                    <Product
+                    id = "1"
+                    title = "boAt Airdopes 441 TWS Ear-Buds with IWP Technology, Immersive Audio, Up to 30H Total Playback, IPX7 Water Resistance, Super Touch Controls, Secure Sports Fit & Type-C Port(Active Black)"
+                    price = {900}
+                    rating= {4}
+                    image="https://m.media-amazon.com/images/I/31LC-FBgpBL._AC_SY200_.jpg" />
+            </div>
+
+            <div className="home__row">
+                <Product
+                        id = "1"
+                        title = "boAt Airdopes 441 TWS Ear-Buds with IWP Technology, Immersive Audio, Up to 30H Total Playback, IPX7 Water Resistance, Super Touch Controls, Secure Sports Fit & Type-C Port(Active Black)"
+                        price = {900}
+                        rating= {4}
+                        image="https://m.media-amazon.com/images/I/31LC-FBgpBL._AC_SY200_.jpg" />
+
+            </div>
         </div>
     )
 }
