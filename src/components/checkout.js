3c175df5e46e18599f2dff98076a12fd94405ceb
ananyamishra12 <66728375+ananyamishra12@users.noreply.github.com>
2021-11-05 19:06:19 +0530

    basket

--- src/components/checkout.js
+++ src/components/checkout.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import useStateValue from './StateProvider'
 
 function checkout() {
+    const [{basket}, dispatch]= useStateValue();
     return (
-        <div>
-            
+        <div className= "checkout">
+            <img 
+                className="checkout__ad"
+            />
         </div>
     )
 }
