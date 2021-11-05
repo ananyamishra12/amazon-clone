c83d9c818913e0e79125d3681a6d9ab104be4fff
ananyamishra12 <66728375+ananyamishra12@users.noreply.github.com>
2021-11-06 03:38:24 +0530

    install

--- src/components/Subtotal.js
+++ src/components/Subtotal.js
@@ -2,8 +2,9 @@ import React from 'react'
 import "./subtotal.css"
 function Subtotal() {
     return (
-        <div>
+        <div className="subtotal">
             <h1>Subtotal</h1>
+            <button>Proceed to checkout</button>
         </div>
     )
 }
